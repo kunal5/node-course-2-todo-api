1b3b8e8d2e2451e053ca0fc58536649d8fcd20b9
Kunal Mahajan <kunalvineeta5@gmail.com>
2018-05-11 03:02:30 +0530

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,17 +1,26 @@
 var env = process.env.NODE_ENV || 'development';
-//console.log('env ****', env);
-var d1 = {
-  dev : 'mongodb://localhost:27017/TodoApp',
-  test : 'mongodb://localhost:27017/TodoAppTest',
-  mlab : 'mongodb://kunal12:apple1234@ds047712.mlab.com:47712/todos'
-};
-if(process.env.PORT){
-  process.env.MONGODB_URI = d1.mlab;
-} else {
-  process.env.PORT = 3000;
-  if(env === 'development'){
-    process.env.MONGODB_URI = d1.dev;
-  } else if (env === 'test'){
-    process.env.MONGODB_URI = d1.test;
-  }
+
+if(env==='development' || env==='test'){
+  var config=require('./config.json');
+  //console.log(config);
+  var envConfig=config[env];
+  Object.keys(envConfig).forEach((key)=>{
+    process.env[key]=envConfig[key];
+  });
 }
+//console.log('env ****', env);
+// var d1 = {
+//   dev : 'mongodb://localhost:27017/TodoApp',
+//   test : 'mongodb://localhost:27017/TodoAppTest',
+//   mlab : 'mongodb://kunal12:apple1234@ds047712.mlab.com:47712/todos'
+// };
+// if(process.env.PORT){
+//   process.env.MONGODB_URI = d1.mlab;
+// } else {
+//   process.env.PORT = 3000;
+//   if(env === 'development'){
+//     process.env.MONGODB_URI = d1.dev;
+//   } else if (env === 'test'){
+//     process.env.MONGODB_URI = d1.test;
+//   }
+// }
